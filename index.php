fcbc118b79fbb7b8fb0bb2481abb87d13caa9026
guilhermedonizetti <guilhermetecnologias@gmail.com>
2022-08-01 14:28:09 -0300

    Pagina para posicionar as estatisticas.

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"> 
+	<meta http-equiv="cache-control" content="max-age=0" />
+	<meta http-equiv="cache-control" content="no-cache" />
+	<meta http-equiv="expires" content="0" />
+	<title>Estatísticas Programador</title>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+</head>
+
+<style type="text/css">
+	body {
+		background-color: #DCDCDC;
+		width: 100%;
+	}
+</style>
+
+<body>
+
+<nav class="navbar navbar-light bg-light">
+  <form class="form-inline">
+    <div class="container">
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">@</span>
+      </div>
+      <input type="text" class="form-control" placeholder="Username do GitHub" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+    </div>
+  </form>
+</nav>
+
+	<br>
+
+	<div class="container" style="justify-content: center;">
+    	<div class="row" >
+        	<div class="col-6">
+				<img src="https://github-readme-stats.vercel.app/api?username=guilhermedonizetti&show_icons=true&theme=merko">
+				<img src="https://github-readme-streak-stats.herokuapp.com?user=guilhermedonizetti&theme=blux&date_format=j%20M%5B%20Y%5D&background=000000" alt="Todas as Contribuicoes no GitHub"/>
+        	</div>
+        	<div class="col-6">
+				<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=guilhermedonizetti&langs_count=10&theme=dark" />
+        	</div>
+    	</div>
+		<div>
+		<img src="https://activity-graph.herokuapp.com/graph?username=guilhermedonizetti&theme=react-dark" alt="Gráfico das contribuições recentes no GitHub">
+		</div>
+	</div>
+
+
+	<br>
+
+	<!-- FOOTER -->
+	<footer class="bg-light text-center text-lg-start">
+		<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+	    	Ver Estatísticas - 
+	    	<a class="text-dark" href="https://github.com/guilhermedonizetti/" target="_blank">Guilherme Donizetti</a>
+	  	</div>
+	</footer>
+
+
+	<!-- SCRIPTS -->
+	<script type="text/javascript">
+
+	</script>
+	<!-- fim scripts -->
+</body>
+</html>
